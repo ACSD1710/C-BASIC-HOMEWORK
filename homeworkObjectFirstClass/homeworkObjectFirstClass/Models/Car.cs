0eeaed73bddcf080e1ae47c158c252efb1002ca8
Darko Aceski <darkoaceski@yahoo.com>
2022-03-24 14:32:21 +0100

    Second Commit

---
+++ homeworkObjectFirstClass/homeworkObjectFirstClass/Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeworkObjectFirstClass.Models
+{
+   public class Car
+    {
+        public string Model { get; set; }
+        public double Speed { get; set; }
+        
+
+        public Car()
+        {
+
+        }
+        public Car(string model, double speed)
+        {
+            Model = model;
+            Speed = speed;
+            
+        }
+
+    }
+}
---
+++ homeworkObjectFirstClass/homeworkObjectFirstClass/Models/Driver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeworkObjectFirstClass.Models
+{
+   public class Driver
+    {
+        public string Name { get; set; }
+        public double Skill { get; set; }
+        
+
+        public Driver()
+        {
+
+        }
+        public Driver(string name, double skill)
+        {
+            Name = name;
+            Skill = skill;
+            
+        }
+
+        public static implicit operator string(Driver v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+}

e25289bab3045d8ce9fad6f95a275089c49c8a06
Darko Aceski <darkoaceski@yahoo.com>
2022-03-27 20:39:24 +0200

    another

---
+++ homeworkObjectFirstClass/homeworkObjectFirstClass/Models/Driver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeworkObjectFirstClass.Models
+{
+   public class Driver
+    {
+        public string Name { get; set; }
+        public double Skill { get; set; }
+        public bool IsChose { get; set; }
+        
+
+        public Driver()
+        {
+
+        }
+        public Driver(string name, double skill, bool isChose)
+        {
+            Name = name;
+            Skill = skill;
+            IsChose = isChose;
+            
+
+        }
+
+      
+    }
+
+    
+}
